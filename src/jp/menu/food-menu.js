218e2a3afde6f1f3d8fc686dc6e5e513e8121ad2
MaceeQi <maceeqi@gmail.com>
2022-12-15 17:00:29 -0500

    food menu contents

--- src/jp/menu/food-menu.js
+++ src/jp/menu/food-menu.js
@@ -1,9 +1,128 @@
 import React from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import foodMenuContents from "../../data/food-menu-contents.json";
 
 const FoodMenuComponent = () => {
+
     return (
         <>
-            Food menu
+            {/* table of contents */}
+            <div className="d-flex flex-wrap justify-content-center">
+                {
+                    foodMenuContents.map(content =>
+                                             <Link to={`#${content}`} className="text-black m-2 ms-3 me-3">
+                                                 {content}
+                                             </Link> )
+                }
+            </div>
+
+            {/* food menu */}
+            <div className="">
+                {/* appetizers */}
+                <div id="Appetizers">
+                    <h2 className="mt-5">Appetizers</h2>
+                </div>
+
+                {/* beef */}
+                <div id="Beef">
+                    <h2 className="mt-5">Beef</h2>
+                </div>
+
+                {/* chef's specialties */}
+                <div id="Chef's Specialties">
+                    <h2 className="mt-5">Chef's Specialties</h2>
+                </div>
+
+                {/* chicken */}
+                <div id="Chicken">
+                    <h2 className="mt-5">Chicken</h2>
+                </div>
+
+                {/* chop suey / chow mein */}
+                <div id="Chop Suey/Chow Mein">
+                    <h2 className="mt-5">Chop Suey/Chow Mein</h2>
+                </div>
+
+                {/* combo plates */}
+                <div id="Combination Plates">
+                    <h2 className="mt-5">Combination Plates</h2>
+                </div>
+
+                {/* egg foo yong */}
+                <div id="Egg Foo Yong">
+                    <h2 className="mt-5">Egg Foo Yong</h2>
+                </div>
+
+                {/* family dinners */}
+                <div id="Family Dinners">
+                    <h2 className="mt-5">Family Dinners</h2>
+                </div>
+
+                {/* fried rice */}
+                <div id="Fried Rice">
+                    <h2 className="mt-5">Fried Rice</h2>
+                </div>
+
+                {/* house specialties */}
+                <div id="House Specialties">
+                    <h2 className="mt-5">House Specialties</h2>
+                </div>
+
+                {/* kiddy kombos */}
+                <div id="Kiddy Kombos">
+                    <h2 className="mt-5">Kiddy Kombos</h2>
+                </div>
+
+                {/* lunch specials */}
+                <div id="Luncheon Specials">
+                    <h2 className="mt-5">Luncheon Specials</h2>
+                </div>
+
+                {/* lo mein */}
+                <div id="Lo Mein">
+                    <h2 className="mt-5">Lo Mein</h2>
+                </div>
+
+                {/* moo shu */}
+                <div id="Moo Shu">
+                    <h2 className="mt-5">Moo Shu</h2>
+                </div>
+
+                {/* pork */}
+                <div id="Pork">
+                    <h2 className="mt-5">Pork</h2>
+                </div>
+
+                {/* rice noodle */}
+                <div id="Rice Noodle">
+                    <h2 className="mt-5">Rice Noodle</h2>
+                </div>
+
+                {/* seafood */}
+                <div id="Seafood">
+                    <h2 className="mt-5">Seafood</h2>
+                </div>
+
+                {/* soups */}
+                <div id="Soups">
+                    <h2 className="mt-5">Soups</h2>
+                </div>
+
+                {/* sweet and sour */}
+                <div id="Sweet and Sour">
+                    <h2 className="mt-5">Sweet and Sour</h2>
+                </div>
+
+                {/* szechuan cuisine */}
+                <div id="Szechuan Cuisine">
+                    <h2 className="mt-5">Szechuan Cuisine</h2>
+                </div>
+
+                {/* vegetarian */}
+                <div id="Vegetarian">
+                    <h2 className="mt-5">Vegetarian</h2>
+                </div>
+            </div>
         </>
     );
 };
